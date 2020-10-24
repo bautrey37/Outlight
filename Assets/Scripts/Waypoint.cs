9aa6a321c653e09601889f5608a48394095b42e7
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-10-24 14:18:58 +0300

    added Waypoint code

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public List<Waypoint> NextWaypoints;
+
+    public Waypoint GetNextWaypoint()
+    {
+        if (NextWaypoints.Count == 0) return null;
+        return NextWaypoints[Random.Range(0, NextWaypoints.Count)];
+    }
+
+    void OnDrawGizmos()
+    {
+        if (NextWaypoints == null) return;
+        Gizmos.color = Color.red;
+        foreach (Waypoint waypoint in NextWaypoints)
+        {
+            Gizmos.DrawLine(transform.position, waypoint.transform.position);
+        }
+
+    }
+}
