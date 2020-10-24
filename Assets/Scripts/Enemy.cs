abff5b22f9b9c4a2a59c8217ab0db85e670e95e8
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-10-24 15:01:20 +0300

    enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,34 +4,78 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    //private List<>
+    private List<Health> TargetsInRange;
+    private float speed = 1f;
+    private Transform target;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        FindClosestTarget();
+        if (target != null)
+        {
+            float step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+
+            if (Vector3.Distance(transform.position, target.position) < 0.001f)
+            {
+                Debug.Log("Reached Target");
+            }
+        }
+    }
+
+    void FindClosestTarget()
+    {
+        if (TargetsInRange == null) return;
+
+        Transform ClosestDistance = null;
+        foreach (Health target in TargetsInRange)
+        {
+            if (ClosestDistance == null)
+            {
+                ClosestDistance = target.transform;
+            }
+            if (Vector3.SqrMagnitude(transform.position - target.transform.position) < Vector3.SqrMagnitude(ClosestDistance.position))
+            {
+                ClosestDistance = target.transform;
+                Debug.Log("Found new closest target");
+            }
+            
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        Health target = collision.GetComponent<Health>();
+        if (target != null)
+        {
+            TargetsInRange.Add(target);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-
+        Health target = collision.GetComponent<Health>();
+        if (target != null)
+        {
+            TargetsInRange.Remove(target);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-
+        Health target = collision.GetComponent<Health>();
+        if (target != null)
+        {
+            if (TargetsInRange.Contains(target))
+            {
+                TargetsInRange.Add(target);
+            }
+        }
     }
-
-
 }
