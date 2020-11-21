52441d05404f2bb0e12f6d95f78d439b4f4bc789
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-11-21 17:36:32 +0200

    add levels menu

--- Assets/Scripts/Menu/ScalingAnimation.cs
+++ Assets/Scripts/Menu/ScalingAnimation.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScalingAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AnimationCurve Curve;
+    public float Speed = 0.5f;
+    public Vector3 StartScale = Vector3.zero;
+    public Vector3 TargetScale = Vector3.one;
+
+    private float timeAggregate;
+
+    public UnityEvent AnimationFinishEvent;
+
+    void OnEnable()
     {
-        
+        timeAggregate = 0;
+        transform.localScale = StartScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timeAggregate += Time.deltaTime * Speed;
+        float value = Curve.Evaluate(timeAggregate);
+        transform.localScale = Vector3.LerpUnclamped(StartScale, TargetScale, value);
+        if (timeAggregate >= 1)
+        {
+            transform.localScale = TargetScale;
+            enabled = false;
+            AnimationFinishEvent.Invoke();
+        }
     }
 }
