76f2fbc91647aba3301a0133a67739cd6294aef9
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-11-21 21:53:58 +0200

    finish level page

--- Assets/Scripts/Menu/LevelsMenu.cs
+++ Assets/Scripts/Menu/LevelsMenu.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayLevel(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
---
+++ Assets/Scripts/Menu/MenuPresenter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPresenter : MonoBehaviour
+{
+    public MainMenu MainMenu;
+    public LevelsMenu LevelsMenu;
+    public OptionsMenu OptionsMenu;
+
+    /*
+     * Tried to get animation to close and open different panels, but it was tricky to get working
+     */
+
+    //public void OpenMainMenu()
+    //{
+    //    if (gameObject.activeSelf)
+    //        return;
+
+    //    MainMenu.enabled = true;
+    //    MainMenu.OpeningAnimation.enabled = true;
+    //}
+
+    //public void OpenLevelsMenu()
+    //{
+    //    if (LevelsMenu.enabled)
+    //        return;
+
+    //    MainMenu.ClosingAnimation.enabled = true;
+    //    MainMenu.enabled = false;
+
+    //    LevelsMenu.OpeningAnimation.enabled = true;
+    //    LevelsMenu.enabled = true;
+    //}
+
+    //public void OpenOptionsMenu()
+    //{
+
+    //}
+
+    //public void Close()
+    //{
+    //    if (!gameObject.activeSelf)
+    //        return;
+
+    //    ClosingAnimation.enabled = true;
+    //}
+
+    //public void CloseFinished()
+    //{
+    //    gameObject.SetActive(false);
+    //}
+}
