2726f87602833b66495ee2a33a1d2a6c61337d6e
Ruslan Parkhomenko <rupark@ttu.ee>
2020-11-22 15:43:16 +0200

    added zoom

---
+++ Assets/Scripts/Game/CameraZoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    private Camera Cam;
+    public float CamSize;
+    public float zoomDelta;
+    float minZoom = 2f;
+    float maxZoom = 10f;
+    void Start()
+    {
+        
+    }
+    void Awake()
+    {
+        // find camera and starting size
+        Cam = Camera.main;
+        CamSize = Cam.orthographicSize;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // get the current size + the scrollwheel change
+        zoomDelta = CamSize + Input.mouseScrollDelta.y;
+        // constrain zoom
+        CamSize = Mathf.Clamp(zoomDelta, minZoom, maxZoom);
+        // apply zoom to field of view
+        Cam.orthographicSize = CamSize;
+    }
+}
