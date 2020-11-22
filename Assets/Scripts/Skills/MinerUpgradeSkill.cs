619bda9a96f34c4e2bc7c58a39591694b24738e5
Ruslan Parkhomenko <rupark@ttu.ee>
2020-11-22 22:33:49 +0200

    added 3rd skill

---
+++ Assets/Scripts/Skills/MinerUpgradeSkill.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class MinerUpgradeSkill : MonoBehaviour
+{
+    public SkillsData skillsData;
+    public Button button;
+    private bool buttonPressed = false;
+    public Miner miner;
+    void Start()
+    {
+        miner.TimeBetweenCoin = 1f;
+        button.onClick.AddListener(Pressed);
+    }
+
+    void Pressed()
+    {
+        buttonPressed = true;
+    }
+    void Update()
+    {
+        if (button.interactable && Input.GetKeyDown((KeyCode)Enum.Parse(typeof(KeyCode), skillsData.Shortcut)) || buttonPressed)
+        {
+            miner.TimeBetweenCoin -= 0.5f;
+            Debug.Log("TimeBetweenCoin" + miner.TimeBetweenCoin);
+            buttonPressed = false;
+        }
+    }
+}
