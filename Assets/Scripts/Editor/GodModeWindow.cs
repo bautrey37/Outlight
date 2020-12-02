23bf75ac7ea970cbc0aa13120aba36acbd8e167c
Miras <kenzhemir@gmail.com>
2020-12-02 16:29:35 +0200

    Add god mode window

---
+++ Assets/Scripts/Editor/GodModeWindow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MyWindow : EditorWindow
+{
+    string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1f;
+
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Window/God Mode")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(MyWindow));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Space(20);
+        GUILayout.Label("God mode settings", EditorStyles.boldLabel);
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("+1000 Gold"))
+        {
+            Events.AddMoney(1000);
+        }
+        GUILayout.Space(5);
+
+        if (GUILayout.Button("Kill all enemies"))
+        {
+            Enemy[] enemies = FindObjectsOfType<Enemy>();
+            foreach (Enemy e in enemies)
+            {
+                GameObject.Destroy(e.gameObject);
+            }
+        }
+        GUILayout.Space(5);
+
+        myFloat = EditorGUILayout.Slider("Slider", myFloat, 0, 2);
+        Time.timeScale = myFloat;
+    }
+}
