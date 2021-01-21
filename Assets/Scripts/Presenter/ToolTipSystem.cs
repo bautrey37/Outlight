2c987ea443553ade04fa64a29a056054a91c4add
Ruslan Parkhomenko <rupark@ttu.ee>
2021-01-21 06:06:59 +0200

    added tooltips for structures

---
+++ Assets/Scripts/Presenter/ToolTipSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipSystem : MonoBehaviour
+{
+    private static ToolTipSystem current;
+
+    public ToolTip toolTip;
+
+    public void Awake()
+    {
+        current = this;
+    }
+    public static void Show(string content, string header = "")
+    {
+        current.toolTip.SetText(content, header);
+        current.toolTip.gameObject.SetActive(true);
+    }
+
+    public static void Hide()
+    {
+        current.toolTip.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Presenter/ToolTipTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToolTipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private static LTDescr delay;
+    public string header;
+
+    [Multiline()]
+    public string content;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        delay = LeanTween.delayedCall(0.5f, () =>
+        {
+            ToolTipSystem.Show(content, header);
+        });
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.cancel(delay.uniqueId);
+        ToolTipSystem.Hide();
+    }
+} 
