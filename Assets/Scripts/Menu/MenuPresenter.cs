5e3c90f248faf9670cf806d1baf6530a772bc119
Brandon Autrey <brandon.c.autrey@gmail.com>
2021-01-21 11:39:06 +0200

    added button clicking sound to menu

--- Assets/Scripts/Menu/MenuPresenter.cs
+++ Assets/Scripts/Menu/MenuPresenter.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuPresenter : MonoBehaviour
 {
     public MainMenu MainMenu;
     public LevelsMenu LevelsMenu;
     public OptionsMenu OptionsMenu;
-    public AudioClipGroup MenuSound;
+    public AudioClipGroup MenuBackgroundSound;
+    public AudioClipGroup ButtonClickSound;
 
     public void Start()
     {
-        MenuSound.PlayBackground();
+        MenuBackgroundSound.PlayBackground();
+        Button[] buttons = GetComponentsInChildren<Button>(true);
+        foreach (Button b in buttons)
+        {
+            b.onClick.AddListener(PlayButtonSound);
+        }
+
+    }
+
+    void PlayButtonSound()
+    {
+        ButtonClickSound.Play();
     }
     /*
      * Tried to get animation to close and open different panels, but it was tricky to get working
